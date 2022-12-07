46e030826d851fcc171bb117b37f3936a273f417
JKSenthil <senthil.jason@gmail.com>
2022-12-06 22:25:59 -0800

    try again

--- src/bin/benchmark.rs
+++ src/bin/benchmark.rs
@@ -2,7 +2,7 @@ use std::sync::Arc;
 
 use ethers::{
     providers::{Middleware, Provider},
-    types::GethDebugTracingOptions,
+    types::{transaction::eip2718::TypedTransaction, GethDebugTracingOptions, TransactionRequest},
 };
 use futures_util::StreamExt;
 
@@ -11,24 +11,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let provider_ipc = Provider::connect_ipc("/home/jsenthil/.bor/data/bor.ipc").await?;
     let provider_ipc = Arc::new(provider_ipc);
 
-    let mut pending_tx_stream = provider_ipc.subscribe_pending_txs().await?;
-    while let Some(tx_hash) = pending_tx_stream.next().await {
-        let b = provider_ipc
-            .debug_trace_transaction(
-                tx_hash,
-                GethDebugTracingOptions {
-                    disable_storage: None,
-                    disable_stack: None,
-                    enable_memory: None,
-                    enable_return_data: None,
-                    tracer: None,
-                    timeout: None,
-                },
-            )
-            .await;
-        match b {
-            Ok(trace) => println!("TRACE: {:?}", trace),
-            Err(e) => println!("ERROR: {:?}", e),
+    let mut pending_tx_stream = provider_ipc
+        .subscribe_pending_txs()
+        .await?
+        .transactions_unordered(16);
+    while let Ok(tx) = pending_tx_stream.next().await.unwrap() {
+        let typed_tx: TypedTransaction = (&tx).into();
+        let result = provider_ipc.call(&typed_tx, None).await;
+        match result {
+            Ok(b) => println!("WORKS! {:?}", b),
+            Err(e) => println!("ERROR! {:?}", e),
         };
     }
 
