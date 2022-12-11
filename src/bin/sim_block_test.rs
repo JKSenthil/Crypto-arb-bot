d328f5bf8913f1ed54a7873569de7e22c2fa51f9
JKSenthil <senthil.jason@gmail.com>
2022-12-10 18:48:42 -0800

    convert to ipc

--- src/bin/sim_block_test.rs
+++ src/bin/sim_block_test.rs
@@ -1,18 +1,15 @@
 use std::sync::Arc;
 
-use ethers::{
-    providers::{Provider, Ws},
-    types::{transaction::eip2718::TypedTransaction, U256},
-};
+use ethers::{providers::Provider, types::U256};
 use tsuki::constants::{protocol::UniswapV2, token::ERC20Token};
 use tsuki::uniswapV2::UniswapV2Client;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let rpc_node_ws_url = std::env::var("ALCHEMY_POLYGON_RPC_WS_URL")?;
-    let provider_ws = Arc::new(Provider::<Ws>::connect(&rpc_node_ws_url).await?);
+    let provider_ipc = Provider::connect_ipc("/home/jsenthil/.bor/data/bor.ipc").await?;
+    let provider_ipc = Arc::new(provider_ipc);
 
-    let client = UniswapV2Client::new(provider_ws.clone());
+    let client = UniswapV2Client::new(provider_ipc.clone());
     let tx = client.get_swapExactTokensForTokens_txn(
         UniswapV2::QUICKSWAP,
         ERC20Token::USDC,
