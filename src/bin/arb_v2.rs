565f1b9bc7716420eba87224d9464e4606e1cca5
JKSenthil <senthil.jason@gmail.com>
2023-01-10 20:59:45 -0800

    updated gas limit prediction algorithm

--- src/bin/arb_v2.rs
+++ src/bin/arb_v2.rs
@@ -43,26 +43,26 @@ lazy_static! {
 // TODO: this doesnt seem to predict accurately?
 // https://github.com/maticnetwork/bor/blob/ad69ccd0ba6aac4a690e6b4778987242609f4845/core/block_validator.go#L108
 fn compute_next_gas_limit(current_gas_limit: U256) -> U256 {
-    let current_gas_limit = current_gas_limit
-        .checked_mul(*ELASTICITY_MULTIPLIER)
-        .unwrap();
-    let delta = current_gas_limit
-        .checked_div(*GAS_LIMIT_BOUND_DIVISOR)
-        .unwrap()
-        - 1;
-    let mut limit = current_gas_limit;
-    if limit < *DESIRED_GAS_LIMIT {
-        limit = current_gas_limit + delta;
-        if limit > *DESIRED_GAS_LIMIT {
-            limit = *DESIRED_GAS_LIMIT;
-        }
-    } else if limit > *DESIRED_GAS_LIMIT {
-        limit = current_gas_limit - delta;
-        if limit < *DESIRED_GAS_LIMIT {
-            limit = *DESIRED_GAS_LIMIT;
-        }
-    }
-    return limit;
+    // let current_gas_limit = current_gas_limit
+    //     .checked_mul(*ELASTICITY_MULTIPLIER)
+    //     .unwrap();
+    // let delta = current_gas_limit
+    //     .checked_div(*GAS_LIMIT_BOUND_DIVISOR)
+    //     .unwrap()
+    //     - 1;
+    // let mut limit = current_gas_limit;
+    // if limit < *DESIRED_GAS_LIMIT {
+    //     limit = current_gas_limit + delta;
+    //     if limit > *DESIRED_GAS_LIMIT {
+    //         limit = *DESIRED_GAS_LIMIT;
+    //     }
+    // } else if limit > *DESIRED_GAS_LIMIT {
+    //     limit = current_gas_limit - delta;
+    //     if limit < *DESIRED_GAS_LIMIT {
+    //         limit = *DESIRED_GAS_LIMIT;
+    //     }
+    // }
+    return current_gas_limit;
 }
 
 // https://github.com/maticnetwork/bor/blob/ad69ccd0ba6aac4a690e6b4778987242609f4845/consensus/misc/eip1559.go#L99
